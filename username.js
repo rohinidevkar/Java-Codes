f178ef99e9d7ae9af4e6ce223c7613c1add106f9
Rohini Devkar <98577401+rohinidevkar@users.noreply.github.com>
2024-02-28 16:58:47 +0530

    Update username.js

--- username.js
+++ username.js
@@ -1,7 +1,20 @@
-let userName = prompt("Enter your name:");
-if (userName !== null && userName !== "") {
-    alert("Hello, " + userName + "!");
-} else {
+<!-- Create a Javascript program that accepts a user name and displays hello with username in the alert box. -->
 
-    alert("Hello there!");
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Greeting Program</title>
+</head>
+<body>
+
+<script>
+
+var userName = prompt("Please enter your name:");
+    alert("Hello, " + userName + "!");
+
+</script>
+
+</body>
+</html>
